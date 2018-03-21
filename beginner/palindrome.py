5341b37e8b1c96f238e026acc7215ee5e9af7cce
sriniklakshman <37336917+sriniklakshman@users.noreply.github.com>
2018-03-21 11:09:00 +0530

    Update palindrome.py

--- beginner/palindrome.py
+++ beginner/palindrome.py
@@ -1,10 +1,13 @@
-x=raw_input()
-temp=x
-a=0
-while(temp!=0):
-	a=a*10+int(temp%10)
-	temp=(temp/10)
-if(a==x):
-	print("palindrome")
+x=int(raw_input())
+if(x<=1000):
+	temp=x
+	a=0
+	while(temp!=0):
+		a=a*10+int(temp%10)
+		temp=(temp/10)
+	if(a==x):
+		print("palindrome")
+	else:
+		print("not palindrome")
 else:
 	print("no")
